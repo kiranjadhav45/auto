4c3bdd9cda6afb71d1a0aa1d03e4d57d6f9a0366
kiran <kiranjadhav9561@gmail.com>
2023-02-14 16:14:44 -0800

    EOD 14 feb

--- src/pages/Sales.js
+++ src/pages/Sales.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SideMenu from "../components/ui/sideMenu";
 import Pagination from "./Pagination";
+import { Link } from "react-router-dom";
 import {
   Button,
   ButtonGroup,
@@ -31,12 +32,20 @@ import {
   CiUser,
   CiViewTimeline,
 } from "react-icons/ci";
+
 import { FcSalesPerformance } from "react-icons/fc";
+import { GrHostMaintenance } from "react-icons/gr";
+import {
+  MdCategory,
+  MdMiscellaneousServices,
+  MdPendingActions,
+} from "react-icons/md";
 
 function Sales() {
   const [pageSize, setPageSize] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchRes, setSearchRes] = useState([]);
+  const [activeSubMenu, setActiveSubMenu] = useState("");
   const [itemList, setItemList] = useState([
     {
       itemName: "Product One",
@@ -149,6 +158,14 @@ function Sales() {
       itemInvoice: 45845,
     },
   ]);
+  const [submenu, setSubMenu] = useState([
+    {
+      category: "",
+      title: "",
+      logo: "",
+      path: "",
+    },
+  ]);
 
   const searchHandle = (e) => {
     let test = itemList.filter((a) => a.itemName === e.target.value);
@@ -166,29 +183,260 @@ function Sales() {
   for (let i = indexOfFirstItem + 1; i <= indexOfLastItem; i++) {
     index.push(i);
   }
+  // let allData = {
+  //   path1: "",
+  //   path2: "",
+  //   path3: "",
+  //   path4: "",
+  //   title1: "",
+  //   title2: "",
+  //   title3: "",
+  //   title4: "",
+  //   logo1: "",
+  //   logo2: "",
+  //   logo3: "",
+  //   logo4: "",
+  // };
+  useEffect(() => {
+    const handleOnSubMenu = async () => {
+      if (activeSubMenu && activeSubMenu === "dashboard") {
+        setSubMenu.title1("Pre-Order");
+        setSubMenu.title2("Total Sales");
+        setSubMenu.title3("pending settlements");
+        setSubMenu.path1("/lastOrders");
+        setSubMenu.path2("/totalSales");
+        setSubMenu.path3("/Maintainance");
+      } else if (activeSubMenu && activeSubMenu === "sales") {
+        setSubMenu.title1("Sales");
+        setSubMenu.title2("Service");
+        setSubMenu.title3("Maintenance");
+        setSubMenu.path1("/lastOrders");
+        setSubMenu.path2("/totalSales");
+        setSubMenu.path3("/Maintainance");
+      } else if (activeSubMenu && activeSubMenu === "inventory") {
+        setSubMenu.title1("Sales");
+        setSubMenu.title2("Service");
+        setSubMenu.title3("Maintenance");
+        setSubMenu.path1("/lastOrders");
+        setSubMenu.path2("/totalSales");
+        setSubMenu.path3("/Maintainance");
+      } else if (activeSubMenu && activeSubMenu === "taxSlab") {
+        setSubMenu.title1("Sales");
+        setSubMenu.title2("Service");
+        setSubMenu.title3("Maintenance");
+        setSubMenu.path1("/lastOrders");
+        setSubMenu.path2("/totalSales");
+        setSubMenu.path3("/Maintainance");
+      } else if (activeSubMenu && activeSubMenu === "hrm") {
+        setSubMenu.title1("Sales");
+        setSubMenu.title2("Service");
+        setSubMenu.title3("Maintenance");
+        setSubMenu.path1("/lastOrders");
+        setSubMenu.path2("/totalSales");
+        setSubMenu.path3("/Maintainance");
+      } else if (activeSubMenu && activeSubMenu === "accounts") {
+        setSubMenu.title1("Sales");
+        setSubMenu.title2("Service");
+        setSubMenu.title3("Maintenance");
+        setSubMenu.path1("/lastOrders");
+        setSubMenu.path2("/totalSales");
+        setSubMenu.path3("/Maintainance");
+      } else if (activeSubMenu && activeSubMenu === "masters") {
+        setSubMenu.title1("Sales");
+        setSubMenu.title2("Service");
+        setSubMenu.title3("Maintenance");
+        setSubMenu.path1("/lastOrders");
+        setSubMenu.path2("/totalSales");
+        setSubMenu.path3("/Maintainance");
+      }
+      console.log("activeSubMenu", activeSubMenu);
+      console.log("SubMenu", submenu);
+    };
+    handleOnSubMenu();
+  }, [activeSubMenu, submenu]);
+  console.log("submenu", submenu);
   return (
     <Container fluid className="bg-white font-ubu">
       <Row className="bg-white border-bottom p-1">
-        <Col className="">
-          <strong size="sm" className="mx-2">
-            N
+        <Col className="col-1">
+          <strong size="sm" className=" m-1 mx-1">
+            Auto
           </strong>
 
-          <button className="headerButton mx-2">
-            <FcSalesPerformance size={20} color="black" />
+          {/* <button className="headerButton mx-2">
+            <FcSalesPerformance size={20} color="dark" />
             <strong className="text-blue m-0 p-0 mx-1 mr-5">Sales</strong>
-          </button>
+          </button> */}
+        </Col>
+        <Col className="col-2 m-1">
+          <Form.Select
+            size="sm"
+            className="Form-select-main"
+            onChange={(e) => setActiveSubMenu(e.target.value)}
+            aria-label="Default select example"
+          >
+            <option value="dashboard">Dashboard</option>
+            <option value="sales">Sales</option>
+            <option value="inventory">Inventory</option>
+            <option value="taxSlab">items</option>
+            <option value="hrm">HRM</option>
+            <option value="accounts">Accounts</option>
+            <option value="masters">Masters</option>
+          </Form.Select>
         </Col>
 
-        <Col className="text-end">
+        <Col className="text-start">
+          <Link to={submenu.path1}>
+            <button className="headerButton mx-2">
+              <FcSalesPerformance size={20} color="black" />
+              <span className="text-black m-0 p-0 mx-1 mr-5">
+                {allData.title1}
+              </span>
+            </button>
+          </Link>
+          <Link to={submenu.path2}>
+            <button className="headerButton mx-2">
+              <FcSalesPerformance size={20} color="black" />
+              <span className="text-black m-0 p-0 mx-1 mr-5">
+                {allData.title2}
+              </span>
+            </button>
+          </Link>
+          <Link to={submenu.path3}>
+            <button className="headerButton mx-2">
+              <MdPendingActions size={20} color="black" />
+              <span className="text-black m-0 p-0 mx-1 mr-5">
+                {allData.title3}
+              </span>
+            </button>
+          </Link>
+        </Col>
+
+        {/* {activeSubMenu && activeSubMenu === "sales" ? (
+          <Col className="text-start">
+            <Link to="/lastOrders">
+              <button className="headerButton mx-2">
+                <FcSalesPerformance size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Sales</span>
+              </button>
+            </Link>
+            <Link to="/totalSales">
+              <button className="headerButton mx-2">
+                <MdMiscellaneousServices size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Service</span>
+              </button>
+            </Link>
+            <Link to="/Maintainance">
+              <button className="headerButton mx-2">
+                <GrHostMaintenance size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">
+                  Maintenance
+                </span>
+              </button>
+            </Link>
+          </Col>
+        ) : (
+          ""
+        )}
+        {activeSubMenu && activeSubMenu === "inventory" ? (
+          <Col className="text-start">
+            <Link to="/lastOrders">
+              <button className="headerButton mx-2">
+                <FcSalesPerformance size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Most Sold</span>
+              </button>
+            </Link>
+            <Link to="/totalSales">
+              <button className="headerButton mx-2">
+                <FcSalesPerformance size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">
+                  Exp Managment
+                </span>
+              </button>
+            </Link>
+            <Link to="/Maintainance">
+              <button className="headerButton mx-2">
+                <MdPendingActions size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">
+                  Rack Managment
+                </span>
+              </button>
+            </Link>
+            <Link to="/Maintainance">
+              <button className="headerButton mx-2">
+                <MdPendingActions size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Dead Stock</span>
+              </button>
+            </Link>
+            <Link to="/Maintainance">
+              <button className="headerButton mx-2">
+                <MdPendingActions size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Returns</span>
+              </button>
+            </Link>
+          </Col>
+        ) : (
+          ""
+        )}
+        {activeSubMenu && activeSubMenu === "hrm" ? (
+          <Col className="text-start">
+            <Link to="/lastOrders">
+              <button className="headerButton mx-2">
+                <FcSalesPerformance size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Customer</span>
+              </button>
+            </Link>
+            <Link to="/totalSales">
+              <button className="headerButton mx-2">
+                <FcSalesPerformance size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Supplier</span>
+              </button>
+            </Link>
+            <Link to="/Maintainance">
+              <button className="headerButton mx-2">
+                <MdPendingActions size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Staff</span>
+              </button>
+            </Link>
+          </Col>
+        ) : (
+          ""
+        )}
+        {activeSubMenu && activeSubMenu === "account" ? (
+          <Col className="text-start">
+            <Link to="/lastOrders">
+              <button className="headerButton mx-2">
+                <FcSalesPerformance size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Customer</span>
+              </button>
+            </Link>
+            <Link to="/totalSales">
+              <button className="headerButton mx-2">
+                <FcSalesPerformance size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Supplier</span>
+              </button>
+            </Link>
+            <Link to="/Maintainance">
+              <button className="headerButton mx-2">
+                <MdPendingActions size={20} color="black" />
+                <span className="text-black m-0 p-0 mx-1 mr-5">Staff</span>
+              </button>
+            </Link>
+          </Col>
+        ) : (
+          ""
+        )} */}
+        <Col className="text-end col-4">
           <button className="headerButton">
             <CiBellOn size={20} color="orange" />
             <small className="text-dark m-0 p-0 mx-1">2000</small>
           </button>
+
           <button className="headerButton">
             <CiStar size={20} color="dodgerblue" />
             <small className="text-dark m-0 p-0 mx-1">120</small>
           </button>
+
           <button className="headerButton position-relative">
             <CiTrophy size={20} color="green" />
             <small className="text-dark m-0 p-0 mx-1">200</small>
